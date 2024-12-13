e5fcf710d939aef171ae7f29443562ed4f722920
MarinaMich <mar7995@yandex.ru>
2024-12-13 17:59:17 +0300

    Реализует слайдер в мобилке

--- source/js/drop-down-menu.js
+++ source/js/drop-down-menu.js
@@ -3,16 +3,19 @@ const menuToggle = document.querySelector('.menu__toggle');
 const body = document.querySelector('.page-body');
 const menuList = document.querySelector('.menu__list');
 const links = Array.from(menuList.querySelectorAll('.menu__item a'));
+const heroSlides = document.querySelector('.hero__slides');
 
 menuToggle.addEventListener('click', () => {
   if (menuMain.classList.contains('menu--closed')) {
     menuMain.classList.remove('menu--closed');
     menuMain.classList.add('menu--opened');
     body.style.overflow = 'hidden';
+    heroSlides.classList.add('hero__slides--dark');
   } else {
     menuMain.classList.add('menu--closed');
     menuMain.classList.remove('menu--opened');
     body.style.overflow = 'auto';
+    heroSlides.classList.remove('hero__slides--dark');
   }
 });
 
@@ -21,5 +24,6 @@ links.forEach((link) => {
     menuMain.classList.add('menu--closed');
     menuMain.classList.remove('menu--opened');
     body.style.overflow = 'auto';
+    heroSlides.classList.remove('hero__slides--dark');
   });
 });
---
+++ source/js/swiper.js
@@ -0,0 +1,30 @@
+// https://swiperjs.com/get-started#installation
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+//swiper.init();
